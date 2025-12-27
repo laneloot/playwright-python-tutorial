09ee86042955ff70a78a2fb02ceb081f78f520ad
laneloot <laneloot03@gmail.com>
2025-12-26 22:36:09 -0500

    feat: add page object and pytest.fixture

---
+++ pages/search.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Page
+
+class DuckDuckGoSearchPage:
+
+    URL = 'https://www.duckduckgo.com'
+
+    def __init__(self, page: Page) -> None:
+        self.page = page
+        self.search_button = page.locator('button.iconButton_button__A_Uiu.searchbox_searchButton__LxebD')
+        self.search_input = page.locator('#searchbox_input')
+    
+    def load(self) -> None:
+        self.page.goto(self.URL)
+    
+    def search(self, phrase: str) -> None:
+        self.search_input.fill(phrase)
+        self.search_button.click()
--- test/test_search.py
+++ test/test_search.py
@@ -1,24 +1,22 @@
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 from playwright.sync_api import expect, Page
 
 def test_basic_duckduckgo_search(page: Page) -> None:
-    # Given the DuckDuckGo home page is displayed
-    page.goto('https://www.duckduckgo.com')
+    search_page = DuckDuckGoSearchPage(page)
+    result_page = DuckDuckGoResultPage(page)
 
-    print('before')
+    # Given the DuckDuckGo home page is displayed
+    search_page.load()
 
     # When the user searches for a phrase
-    page.locator('#searchbox_input').fill('panda')
-    page.locator('button.iconButton_button__A_Uiu.searchbox_searchButton__LxebD').click()
+    search_page.search('panda')
 
     # Then the search result query is the phrase
-    expect(page.locator('#search_form_input')).to_have_value('panda')
+    expect(result_page.search_input).to_have_value('panda')
 
     # And the search result links pertain to the phrase
-    page.locator('a[data-testid="result-title-a"]').nth(4).wait_for()
-    titles = page.locator('a[data-testid="result-title-a"]').all_text_contents()
-    print('titles')
-    print(titles)
-    matches = [t for t in titles if 'panda' in t.lower()]
-    assert len(matches) > 0
+    assert result_page.result_link_titles_contain_phrase('panda')
 
+    # And the search result title contains the phrase
     expect(page).to_have_title('panda at DuckDuckGo')
